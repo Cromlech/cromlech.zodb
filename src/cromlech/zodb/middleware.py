dd8a64b44273b3e7bf8d9752c050586dc6c039f3
Souheil CHELFOUH <trollfot@gmail.com>
2012-01-04 18:04:56 +0100

    Corrected inversed params

--- src/cromlech/zodb/middleware.py
+++ src/cromlech/zodb/middleware.py
@@ -24,20 +24,22 @@ def zodb_app_factory(app, db, key,
 
 
 def zodb_filter_middleware(
+    app,
     global_conf,  # A dict containing the ['DEFAULT'] section of the ini.
-    app, configuration, initializer, key="zodb.connection",
+    configuration, initializer=None, key="zodb.connection",
     transaction=True, transaction_key='transaction.manager'):
     """This middleware factory is compatible with the filter_app
     prototype from `PasteDeploy`. It can, however, be used with
     any WSGI server if properly called. For a less specific implementation
     have a look at ``cromlech.zodb.middleware:zodb_app_factory``.
     """
-    # initializer is a string: py_module_path:class_or_function
-    initializer = eval_loader(initializer)
+    if initializer is not None:
+        # initializer is a string: py_module_path:class_or_function
+        initializer = eval_loader(initializer)
 
     # configuration is an XML-based ZODB conf
     db = init_db(configuration, initializer)
-    
+
     return zodb_app_factory(
         app, db, key,
         transaction in (True, 'true', 'y', 'yes'),
