5b1d05b02b098eefad882f8bf0fd5fb2702cd50e
Alex Garel <alex.garel@gmail.com>
2012-11-16 17:11:27 +0100

    added test for iterating response and calling close

--- src/cromlech/zodb/tests/test_middleware.py
+++ src/cromlech/zodb/tests/test_middleware.py
@@ -22,3 +22,36 @@ def test_middleware_simple():
     app = TestApp(ZODBApp(simple_app, db, 'zodb'))
 
     assert app.get('/').body == 'running !'
+
+
+class IterResponse(object):
+
+    closed = False
+
+    def __iter__(self):
+        yield '3, '
+        yield '2, '
+        yield '1, '
+        yield 'FIRE'
+
+    def close(self):
+        self.closed = True
+
+
+def test_middleware_iter_and_close():
+    """
+    test middleware with iteration and close
+    """
+    iter_response = IterResponse()
+
+    def iter_app(environ, start_response):
+        start_response('200 OK', [('Content-Type', 'plain/text')])
+        return iter_response
+
+    db = DB(DemoStorage())
+    make_app(db)
+    app = TestApp(ZODBApp(iter_app, db, 'zodb'))
+
+    assert not iter_response.closed
+    assert app.get('/').body == '3, 2, 1, FIRE'
+    assert iter_response.closed
